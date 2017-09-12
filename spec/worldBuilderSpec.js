4589f5c70a1786eda6adf0926325b0b8e9a7d14f
Imogen Hardy <imogen.hardy@gmail.com>
2017-09-12 12:13:34 +0100

    fixed travis issue

--- spec/worldBuilderSpec.js
+++ spec/worldBuilderSpec.js
@@ -1,34 +1,34 @@
-describe('WorldBuilder', () => {
-  var jimmy
+describe('WorldBuilder', function () {
+  var jimmy;
   var canvas = {
     width: 512,
     height: 256
-  }
+  };
 
   beforeEach(function () {
-    jimmy = new WorldBuilder(canvas)
-  })
+    jimmy = new WorldBuilder(canvas);
+  });
 
-  describe('#setGrid', () => {
+  describe('#setGrid', function () {
     it('creates an array based on the canvas height', function () {
-      jimmy.setGrid()
-      expect(jimmy.getGrid().length).toEqual(256/8)
-    })
+      jimmy.setGrid();
+      expect(jimmy.getGrid().length).toEqual(256/8);
+    });
 
     it('contains rows full of zeros', function () {
-      jimmy.setGrid()
-      expect(jimmy.getGrid()[0][0]).toEqual(0)
-    })
+      jimmy.setGrid();
+      expect(jimmy.getGrid()[0][0]).toEqual(0);
+    });
 
     it('contains rows of a length based on the canvas width', function () {
-      jimmy.setGrid()
-      expect(jimmy.getGrid()[0].length).toEqual(512/16)
-    })
-  })
+      jimmy.setGrid();
+      expect(jimmy.getGrid()[0].length).toEqual(512/16);
+    });
+  });
 
-  describe('#setRow', () => {
+  describe('#setRow', function () {
     it('creates an array based on the canvas width', function () {
-      expect(jimmy.setRow().length).toEqual(512/16)
-    })
-  })
-})
+      expect(jimmy.setRow().length).toEqual(512/16);
+    });
+  });
+});
