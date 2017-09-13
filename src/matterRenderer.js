4a5f7c3fb8145ef40e2c0685282bccb4f045ad4e
paulmillen <pwmillen@gmail.com>
2017-09-13 17:41:16 +0100

    added events, player sensor to managing jumping.  Removed Matter Render to render with ctx functions

--- src/matterRenderer.js
+++ src/matterRenderer.js
@@ -7,36 +7,57 @@ var Engine = Matter.Engine,
 
 // for reference...
 
-// const KEY_W = 87;
-// const KEY_A = 65;
-// const KEY_S = 83;
-// const KEY_D = 68;
-// const KEY_SPACE = 32;
-// const KEY_SHIFT = 16;
+const KEY_W = 87;
+const KEY_A = 65;
+const KEY_S = 83;
+const KEY_D = 68;
 
 // movement
 
+const keys = [];
+var playerOnFloor = false;
+
+
+document.body.addEventListener('keyup', function(e) {
+  keys[e.keyCode] = false;
+});
+
 document.body.addEventListener('keydown', function(e) {
-  console.log(e.keyCode)
-
-  if((e.keyCode === 87)) {
-      let force = (-0.05 * player.mass);
-      Body.applyForce(player,player.position,{x:0,y:force});
-  };
-
-  if(e.keyCode === 68) {
-      let force = (0.01 * player.mass);
-      Body.applyForce(player,player.position,{x:force,y:0});
-  };
-
-  if(e.keyCode === 65) {
-      let force = (-0.01 * player.mass);
-      Body.applyForce(player,player.position,{x:force,y:0});
-  };
+  keys[e.keyCode] = true;
 });
 
 // create an engine
 var engine = Engine.create();
+
+// watching for position of player
+
+Events.on(engine, 'collisionEnd', function(event) {
+    var pairs = event.pairs;
+
+    for (var i = 0, j = pairs.length; i != j; ++i) {
+        var pair = pairs[i];
+
+        if (pair.bodyA === playerFloorSensor) {
+            playerOnFloor = false;
+        } else if (pair.bodyB === playerFloorSensor) {
+            playerOnFloor = false;
+        };
+    };
+});
+
+ Events.on(engine, 'collisionActive', function(event) {
+    var pairs = event.pairs;
+
+    for (var i = 0, j = pairs.length; i != j; ++i) {
+        var pair = pairs[i];
+
+        if (pair.bodyA === playerFloorSensor) {
+            playerOnFloor = true;
+        } else if (pair.bodyB === playerFloorSensor) {
+            playerOnFloor = true;
+        };
+    };
+});
 
 // create a renderer
 var render = Render.create({
@@ -91,11 +112,16 @@ for (var i = 0; i < worldBuilder.canvas.height / bHeight; i++) {
 
 // created playerBody
 var playerBody = Bodies.rectangle(30,0,20,20, { density: 0.002, friction: 0.5 });
+var playerNeck = Bodies.circle(15,0,5,{ density: 0.002, friction: 0.5 });
+var playerHead = Bodies.rectangle(5,0,10,10, { density: 0.002, friction: 0.5 });
+var playerFloorSensor = Bodies.circle(30,0,10,{density:0, friction:0.3, isSensor: true});
 // create player
 var player = Body.create({
-  parts: [playerBody],
-  friction: 0
+  parts: [playerBody, playerNeck, playerHead, playerFloorSensor],
+  friction: 5
 });
+
+Body.setAngle(player, 0);
 
 createdBodies.push(player)
 // created starting platform for player
@@ -104,7 +130,7 @@ createdBodies.push(startingPlatform)
 // create two boxes and a ground
 createdBodies.push(Bodies.rectangle(350, 0, 40, 40, objectOptions));
 createdBodies.push(Bodies.rectangle(550, 0, 40, 40, objectOptions));
-// createdBodies.push(Bodies.rectangle(512, 512, 1024, 20, { isStatic: true }));
+createdBodies.push(Bodies.rectangle(512, 512, 1024, 20, { isStatic: true }));
 
 // add all of the bodies to the world
 World.add(engine.world, createdBodies);
@@ -113,4 +139,41 @@ World.add(engine.world, createdBodies);
 Engine.run(engine);
 
 // run the renderer
-Render.run(render);
+(function render() {
+  Body.setAngle(player, 0);
+
+  if(keys[KEY_W] && playerOnFloor) {
+      let force = (-0.05 * player.mass) ;
+      Body.applyForce(player,player.position,{x:0,y:force});
+  }
+
+  if(keys[KEY_D]){
+      let force = (0.0004 * player.mass) ;
+      Body.applyForce(player,player.position,{x:force,y:0});
+  }
+  if(keys[KEY_A]){
+      let force = (-0.0004 * player.mass) ;
+      Body.applyForce(player,player.position,{x:force,y:0});
+  }
+
+  window.requestAnimationFrame(render);
+
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.beginPath();
+  for (var i = 0; i < createdBodies.length; i += 1) {
+      var vertices = createdBodies[i].vertices;
+      ctx.moveTo(vertices[0].x, vertices[0].y);
+      for (var j = 1; j < vertices.length; j += 1) {
+          ctx.lineTo(vertices[j].x, vertices[j].y);
+      }
+      ctx.lineTo(vertices[0].x, vertices[0].y);
+  };
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = '#000';
+  ctx.stroke();
+
+})();
