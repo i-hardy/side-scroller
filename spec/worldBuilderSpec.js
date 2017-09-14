28a68d0bd7aae094e94336f0185653ff1a2c01f8
Petra Chocholova <petra.chocholova@gmail.com>
2017-09-14 19:06:08 +0200

    Jasmine test correction after merge

--- spec/worldBuilderSpec.js
+++ spec/worldBuilderSpec.js
@@ -93,8 +93,8 @@ describe('WorldBuilder', function () {
 
   describe('#setGround', function () {
     it('sets final row in the grid to 1, 2, 3 or 4', function () {
-      expect(jimmy.getGrid()[31][10]).toBeGreaterThan(0);
-      expect(jimmy.getGrid()[31][10]).toBeLessThan(5);
+      expect(jimmy.getGrid()[7][3]).toBeGreaterThan(0);
+      expect(jimmy.getGrid()[7][3]).toBeLessThan(5);
     });
   });
 });
