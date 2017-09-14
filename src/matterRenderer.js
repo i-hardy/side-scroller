20d8f4c9705bc3238e2e09a6f5af6bbd02a7f256
Imogen Hardy <imogen.hardy@gmail.com>
2017-09-14 10:43:57 +0100

    beginning integration of scroll

--- src/matterRenderer.js
+++ src/matterRenderer.js
@@ -135,6 +135,12 @@ createdBodies.push(Bodies.rectangle(512, 512, 1024, 20, { isStatic: true }));
 // add all of the bodies to the world
 World.add(engine.world, createdBodies);
 
+// make the world bounds a little bigger than the render bounds
+engine.world.bounds.min.x = 0;
+engine.world.bounds.min.y = 0;
+engine.world.bounds.max.x = 3072;
+engine.world.bounds.max.y = render.options.height;
+
 // run the engine
 Engine.run(engine);
 
@@ -160,6 +166,53 @@ Engine.run(engine);
 
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
+
+  // get the centre of the viewport
+  var viewportCentre = {
+      x: canvas.width * 0.5,
+      y: canvas.height * 0.5
+  };
+
+  // use the engine tick event to control our view
+  Events.on(engine, 'beforeTick', function() {
+      var world = engine.world,
+          mouse = mouseConstraint.mouse,
+          translate;
+
+      // get vector from mouse relative to centre of viewport
+      var deltaCentre = Vector.sub(mouse.absolute, viewportCentre);
+           // Use centreDist > 50 to enable two-way scroll
+          // centreDist = Vector.magnitude(deltaCentre);
+
+      // translate the view if mouse has moved over 50px from the centre of viewport
+      if (deltaCentre.x > 50) {
+          // create a vector to translate the view, allowing the user to control view speed
+          var direction = Vector.normalise(deltaCentre),
+              speed = 2;
+
+          translate = Vector.mult(direction, speed);
+
+          // prevent the view moving outside the world bounds
+          if (render.bounds.min.x + translate.x < world.bounds.min.x)
+              translate.x = world.bounds.min.x - render.bounds.min.x;
+
+          if (render.bounds.max.x + translate.x > world.bounds.max.x)
+              translate.x = world.bounds.max.x - render.bounds.max.x;
+
+          if (render.bounds.min.y + translate.y < world.bounds.min.y)
+              translate.y = world.bounds.min.y - render.bounds.min.y;
+
+          if (render.bounds.max.y + translate.y > world.bounds.max.y)
+              translate.y = world.bounds.max.y - render.bounds.max.y;
+
+          // move the view
+          Bounds.translate(render.bounds, translate);
+
+          // we must update the mouse too
+          Mouse.setOffset(mouse, render.bounds.min);
+      }
+  });
+
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
