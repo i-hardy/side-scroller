a70e78a6e493b18721a87c1bdf3da4a7806cfa40
Costas Kiteou <ckiteou22@gmail.com>
2017-09-14 14:49:27 +0100

    End bonus is awarded when cat reached end of frame

--- spec/scoreSpec.js
+++ spec/scoreSpec.js
@@ -1,9 +1,11 @@
 describe('Score', function () {
 
 var hermione;
+var cat;
 
 beforeEach(function () {
   hermione = new Score();
+  cat = jasmine.createSpyObj('cat', ['getX']);
 });
 
 describe('#points', function () {
@@ -15,6 +17,14 @@ describe('#points', function () {
     it('increases points when called', function () {
       hermione.increase(100);
       expect(hermione.points).toEqual(100);
+    });
+  });
+
+  describe('end of level bonus', function () {
+    it('it awards end of level bonus if end is reached', function () {
+      cat.getX.and.returnValue(512);
+      hermione.endBonus();
+      expect(hermione.points).toEqual(hermione.END_BONUS);
     });
   });
 });
--- src/score.js
+++ src/score.js
@@ -1,7 +1,12 @@
 function Score() {
   this.points = 0
+  this.END_BONUS = 100;
 }
 
 Score.prototype.increase = function (points) {
-  this.points += points
+  this.points += points;
+};
+
+Score.prototype.endBonus = function () {
+  this.increase(this.END_BONUS);
 };
