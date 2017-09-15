521279d1bd5e5dd32c299b40104897dfeaa2da4a
Petra Chocholova <petra.chocholova@gmail.com>
2017-09-15 17:39:39 +0200

    Left and right viewport borders

--- src/renderer.js
+++ src/renderer.js
@@ -5,9 +5,9 @@ function Renderer(player, world) {
   this.ctx = canvas.getContext('2d');
   this.viewport = {
       centreX: worldOptions.viewWidth * 0.5,
-      rightEdge: worldOptions.viewWidth
+      rightEdge: worldOptions.viewWidth,
+      leftEdge: 0
   };
-  this.increment = 0;
 }
 
 Renderer.prototype.playerMovement = function () {
@@ -17,11 +17,36 @@ Renderer.prototype.playerMovement = function () {
   this.player.moveRight();
 };
 
+Renderer.prototype.checkLeftBorder = function () {
+  var playerX = this.player.getBodyObject().bounds.min.x;
+  if (playerX < this.viewport.leftEdge) {
+    var horizonalVelocity = this.player.getBodyObject().velocity.x;
+    Matter.Body.applyForce(this.player.getBodyObject(),
+                            this.player.getBodyObject().position,
+                            {x:(-horizonalVelocity/50), y:0})
+                            }
+};
+
+Renderer.prototype.checkRightBorder = function () {
+  var playerX = this.player.getBodyObject().bounds.max.x;
+  if (playerX > this.viewport.rightEdge) {
+    var horizonalVelocity = this.player.getBodyObject().velocity.x;
+    Matter.Body.applyForce(this.player.getBodyObject(),
+                            this.player.getBodyObject().position,
+                            {x:(-horizonalVelocity/50), y:0})
+                            }
+};
+
+Renderer.prototype.checkBorder = function () {
+  this.checkLeftBorder();
+  this.checkRightBorder();
+};
+
 Renderer.prototype.scroll = function () {
   var playerX = this.player.getBodyObject().position.x;
   var worldEdge = this.world.bounds.max.x;
   if (playerX > this.viewport.centreX && worldEdge > this.viewport.rightEdge) {
-    this.increment += worldOptions.scrollIncrement;
+    this.viewport.leftEdge += worldOptions.scrollIncrement;
     this.viewport.centreX += worldOptions.scrollIncrement;
     this.viewport.rightEdge += worldOptions.scrollIncrement;
   }
@@ -30,11 +55,12 @@ Renderer.prototype.scroll = function () {
 Renderer.prototype.updateScreen = function () {
   Matter.Body.setAngle(this.player.getBodyObject(), 0);
   this.playerMovement();
+  this.checkBorder();
   this.scroll();
   var bodies = this.world.bodies;
 
   this.ctx.clearRect(0, 0, worldOptions.viewWidth, worldOptions.height);
-  this.ctx.translate(-this.increment, 0);
+  this.ctx.translate(-this.viewport.leftEdge, 0);
 
   this.ctx.beginPath();
   for (var i = 0; i < bodies.length; i += 1) {
