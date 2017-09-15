e3229a42d4bb86be0ea656c943bf4b868284f308
Costas Kiteou <ckiteou22@gmail.com>
2017-09-16 00:04:29 +0100

    Removed extra line for codeclimate

--- src/randomNumberGenerator.js
+++ src/randomNumberGenerator.js
@@ -1,8 +1,6 @@
 (function(exports) {
-
   function randomNumberGenerator(min, max) {
     return Math.floor(Math.random()*(max-min) + min);;
   };
-
   exports.randomNumberGenerator = randomNumberGenerator;
 })(this);
