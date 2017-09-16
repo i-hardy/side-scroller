2b2c7d94485c3c4ff62853101037ee1546942cc3
Costas Kiteou <ckiteou22@gmail.com>
2017-09-16 08:33:48 +0100

    Fixing codeclimate issues

--- src/randomNumberGenerator.js
+++ src/randomNumberGenerator.js
@@ -1,6 +1,6 @@
 (function(exports) {
   function randomNumberGenerator(min, max) {
-    return Math.floor(Math.random()*(max-min) + min);;
-  };
+    return Math.floor(Math.random()*(max-min) + min);
+  }
   exports.randomNumberGenerator = randomNumberGenerator;
 })(this);
