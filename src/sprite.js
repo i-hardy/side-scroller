98bd62c7fb9bc1c6415c951ef99ff0bda79a6a94
paulmillen <pwmillen@gmail.com>
2017-09-18 16:46:43 +0100

    jump frame added

--- src/sprite.js
+++ src/sprite.js
@@ -3,19 +3,21 @@
 
 	function Sprite() {
 		this.image = new Image();
-		this.image.src = "assets/images/cats_sprite.png";
+		this.image.src = "assets/images/cats_sprite1.png";
 		this.width = 70;
 		this.height = 70;
-		this.numberOfFrames = 5;
+		this.numberOfFrames = 4;
 		this.currentFrame = 2;
 		this.separation = 70;
-		this.frameIndexes = [[3,2,1,0], [1, 2, 3, 4],[4]];
+    this.direction = 1
+		this.frameIndexes = [[3, 2, 1, 0, 5], [1, 2, 3, 4, 5]]
 	};
 
 	Sprite.prototype.getDirection = function () {
-		if (keys[KEY_A]) {return 0};
-		if (keys[KEY_D]) {return 1}
-    if (!keys[KEY_A] && !keys[KEY_D]) { this.currentFrame = 1; return 1 }
+		if (keys[KEY_A]) { return this.direction = 0 };
+		if (keys[KEY_D]) { return this.direction = 1 };
+    if (keys[KEY_W]) { this.currentFrame = 4; return this.direction }
+    if (!keys[KEY_A] && !keys[KEY_D]) { this.currentFrame = 1; return this.direction }
 	};
 
 	Sprite.prototype.update = function () {
