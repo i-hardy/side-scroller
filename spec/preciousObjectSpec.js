b6dc0012a9bd7572a1e338f10c7f6df7b6230aff
Petra Chocholova <petra.chocholova@gmail.com>
2017-09-19 19:54:33 +0200

    more tests

--- spec/preciousObjectSpec.js
+++ spec/preciousObjectSpec.js
@@ -1,17 +1,17 @@
 'use strict';
 
 describe('PreciousObject', function() {
-  var preciousObject;
+  var mrFancyPants;
 
   beforeEach(function() {
     spyOn(window, 'randomNumberGenerator').and.returnValue(2);
     spyOn(PreciousObject.prototype, 'createBody').and.callThrough();
-    preciousObject = new PreciousObject(100, 150);
+    mrFancyPants = new PreciousObject(100, 150);
   });
 
   describe('#instantiation', function() {
     it('has a preciousness', function() {
-      expect(preciousObject.preciousness).toBeDefined();
+      expect(mrFancyPants.preciousness).toBeDefined();
     });
 
     it('creates a Matter Body', function () {
@@ -21,36 +21,36 @@ describe('PreciousObject', function() {
 
   describe('#getPreciousness', function () {
     it('returns the preciousness', function () {
-      expect(preciousObject.getPreciousness()).toEqual(2);
+      expect(mrFancyPants.getPreciousness()).toEqual(2);
     });
   });
 
   describe('#getBody', function () {
     it('returns the Matter Body relating to the precious object', function () {
       spyOn(Matter.Bodies, 'rectangle').and.returnValue({name: 'Body'});
-      preciousObject.createBody();
-      expect(preciousObject.getBody()).toEqual({name: 'Body'});
+      mrFancyPants.createBody();
+      expect(mrFancyPants.getBody()).toEqual({name: 'Body'});
     });
   });
 
   describe('#createBody', function () {
     it('generates a Matter Body at its x and y coordinates', function () {
       spyOn(Matter.Bodies, 'rectangle').and.callThrough();
-      preciousObject.createBody();
+      mrFancyPants.createBody();
       expect(Matter.Bodies.rectangle).toHaveBeenCalled();
     });
   });
 
   describe('#isOnFloor', function () {
     it('returns whether or not the object is on the floor', function () {
-      expect(preciousObject.isOnFloor()).toBe(false);
+      expect(mrFancyPants.isOnFloor()).toBe(false);
     });
   });
 
   describe('#fallen', function() {
     it('sets onFloor to true', function() {
-      preciousObject.fallen();
-      expect(preciousObject.isOnFloor()).toBe(true);
+      mrFancyPants.fallen();
+      expect(mrFancyPants.isOnFloor()).toBe(true);
     });
   });
 });
