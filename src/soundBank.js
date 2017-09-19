a7de0a6937ec6175710585efb2b19cd5e9008bf5
paulmillen <pwmillen@gmail.com>
2017-09-19 17:49:40 +0100

    added game theme

--- src/soundBank.js
+++ src/soundBank.js
@@ -3,18 +3,21 @@
 function SoundBank() {
   this.scamper = new Audio('./assets/sounds/scurrying.wav')
   this.meow1 = new Audio('./assets/sounds/meow1.wav')
-  this.meow2 = new Audio('./assets/sounds/meow2.wav')
-  this.meow3 = new Audio('./assets/sounds/meow3.wav')
+  this.gameTheme = new Audio('./assets/sounds/gametheme.mp3')
 };
 
 SoundBank.prototype.setPlayerVols = function () {
   this._setMeowVol();
   this._setScamperVol();
+  this._setThemeVol();
 };
 
+SoundBank.prototype._setThemeVol = function() {
+  this.gameTheme.volume = 0.2;
+}
+
 SoundBank.prototype._setMeowVol = function() {
-  var jumpMeows = [this.meow1, this.meow2, this.meow3];
-  for (var i = 0; i < jumpMeows.length; i++) { jumpMeows[i].volume = 0.3 };
+  this.meow1.volume = 0.3;
 };
 
 SoundBank.prototype._setScamperVol = function() {
